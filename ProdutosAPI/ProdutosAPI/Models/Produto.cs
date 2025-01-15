4333b05d00201104224587b44f7b357500934512
Rodrigo Zegóbia <rodrigozegobia@gmail.com>
2025-01-15 20:41:09 -0300

    Upload projeto

---
+++ ProdutosAPI/ProdutosAPI/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿namespace ProdutosAPI.Models
+{
+    public class Produto
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); // Id gerado automaticamente
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string Descricao { get; set; }
+
+
+        public Produto(Guid id, string nome, decimal preco, string descricao)
+        {
+            Id = id;
+            Nome = nome;
+            Descricao = descricao;
+            Preco = preco;
+        }
+
+    }
+}
---
+++ ProdutosAPI/ProdutosAPI/Rotas/ProdutoRotas.cs
@@ -0,0 +1,28 @@
+﻿using ProdutosAPI.Models;
+
+namespace ProdutosAPI.Rotas
+{
+    public static class ProdutoRotas
+    {
+        public static List<Produto> Produtos = new List<Produto>()
+        {
+            new(Guid.NewGuid(), "Brinquedo", 10,""),
+            new(Guid.NewGuid(), "Bola", 20,"")
+        };
+
+        public static void MapProdutoRotas(this WebApplication app)
+        {
+            app.MapGet("/produtos", () => Produtos);
+
+            app.MapGet("/produtos/{nome}", (string nome) => Produtos.Find(x => x.Nome.StartsWith(nome)));
+
+            app.MapPost("/produtos",
+                (HttpContext request, Produto produto) =>
+            {
+                produto.Id = Guid.NewGuid();
+                Produtos.Add(produto);
+                return Results.Ok(produto);
+            });
+        }
+    }
+}
